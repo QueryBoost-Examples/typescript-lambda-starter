b620d7d7a4feadf0b78730cf02d0aaa55b7affb0
David <dtegart78@gmail.com>
2020-08-19 16:01:13 +0200

    changes for deployment

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -1,7 +1,3 @@
-/*  Example Query for the stripe namespace
- returns the the date the customer was created and pageInfo
- created uses the format argument to convert the timestamp to a date in YYYY-MM format
- pageInfo returns data that can be used in pagination */
 import { gql } from 'graphql-request';
 
 export const GET_CUSTOMERS = gql`
@@ -9,7 +5,7 @@ export const GET_CUSTOMERS = gql`
     stripe {
       customers(input: { pagination: { startingAfter: $startingAfter } }) {
         data {
-          created(format: " YYYY-MM")
+          created(format: "YYYY-MM")
         }
         pageInfo {
           totalRecords
--- src/index.ts
+++ src/index.ts
@@ -3,16 +3,14 @@ import { GraphQLClient } from 'graphql-request';
 import { GET_CUSTOMERS } from './graphql/query';
 import { GetCustomersQuery, GetCustomersQueryVariables } from './types/types';
 
-//Hander for triggering the Lambda from an AWS Cloudwatch event
 export const eventHandler: ScheduledHandler = async (event) => {
+  //Handler for triggering the Lambda from an AWS Cloudwatch event
   //event contains the time that Cloudwatch triggered the lambda. Can be used as an argument in queries.
-  console.log(event.time);
 
   //Endpoint for QueryBoost. Replace with https://api.queryboost.com for your live data
   const endpoint = 'https://api.queryboost.com/mocks';
 
   //initialize the graphql client for making requests
-
   const graphQLClient = new GraphQLClient(endpoint, {
     headers: {
       authorization: process.env.MOCK_API_KEY!, //Replace with your API Key
@@ -25,15 +23,16 @@ export const eventHandler: ScheduledHandler = async (event) => {
   while (hasMore) {
     try {
       const data: GetCustomersQuery = await graphQLClient.request(GET_CUSTOMERS, variables);
-      //do something with the data
+      // do something with the data
       console.log(data.stripe.customers?.data);
 
       //handle pagination
       const pageInfo = data.stripe.customers?.pageInfo;
+
       variables = { startingAfter: pageInfo?.endCursor };
       hasMore = pageInfo?.hasMore ? pageInfo?.hasMore : false;
     } catch (error) {
-      console.log(error);
+      console.error(error);
     }
   }
 };
